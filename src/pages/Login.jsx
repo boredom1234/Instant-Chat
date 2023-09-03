0539c887150440d50f3febac3af2e4ea44c54536
boredom1234 <roughuse82@gmail.com>
2023-09-03 12:50:13 +0530

    themes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,9 +25,11 @@ const Login = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Login 📦</h1>
+          <h1 className="text-5xl font-bold">
+            Login <span className="loading loading-ring loading-sm"></span>
+          </h1>
           <p className="py-6">
-<br></br>
+            {/* <video src="src\pages\video.mp4" loop autoplay controls></video> */}
           </p>
           <button onClick={handleLogin} className="btn btn-primary">
             Login with Google
